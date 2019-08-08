3c78d922c810a9f469e45539c318e6e5aa84bb97
algor1 <alexeev.igor.v@gmail.com>
2019-08-08 11:21:42 +0300

    no message

--- WingServer.Tests/ShipTests.cs
+++ WingServer.Tests/ShipTests.cs
@@ -10,26 +10,29 @@ namespace WingServer.Tests
     public class ShipTests
     {
         [Test]
-        [TestCase (1,0,0 , 1,0,0)]
-        [TestCase (0,0,1 , 0,0,1)]
-        public void Move_Speed1Direction_CorrectDestination(float directionx, float directiony, float directionz, float expectedx , float expectedy, float expectedz)
+        [TestCase(1, 0, 0, 1, 0, 0)]
+        [TestCase(0, 0, 1, 0, 0, 1)]
+        public void Move_Speed1Direction_CorrectDestination(float directionx, float directiony, float directionz, float expectedx, float expectedy, float expectedz)
         {
-       
+
             MyQuaternion rotation = new MyQuaternion();
             Vector3 direction = new Vector3(directionx, directiony, directionz);
             Ship sut = new Ship(new ShipData());
             rotation.SetLookRotation(direction);
             sut.Data.Rotation = rotation;
-                
+
             sut.Data.Speed = 1f;
             sut.Tick();
             Vector3 actual = sut.Data.Position;
-            Assert.That(actual.x, Is.EqualTo(expectedx).Within(0.1),"Wrong value in X");
-            Assert.That(actual.y, Is.EqualTo(expectedy).Within(0.1),"Wrong value in Y");
+            Assert.That(actual.x, Is.EqualTo(expectedx).Within(0.1), "Wrong value in X");
+            Assert.That(actual.y, Is.EqualTo(expectedy).Within(0.1), "Wrong value in Y");
             Assert.That(actual.z, Is.EqualTo(expectedz).Within(0.1), "Wrong value in Z");
         }
         [Test]
-        public class Accelerate_SpeedAcceleration_CorrectSpeed()
+        public void Accelerate_SpeedAcceleration_CorrectSpeed()
+            {
+            Ship sut = new Ship(new ShipData());
 
+    }
     }
 }
